1b0c31602cd6396b92f83c4d685d5311f9d7382a
Tyler <tyler.hagstrom@gmail.com>
2021-09-12 10:14:32 -0500

    Reset button now lets you choose a grid size

--- etcher.js
+++ etcher.js
@@ -10,30 +10,51 @@ function addHover(item){
     });
 }
 
-function resetGrid(){
+/*function resetGrid(){
+    createGrid();
     let gridItems = document.querySelectorAll('.gridSq');
     gridItems.forEach((e) => {e.style['background-color'] = startColor});
+}*/
+
+function getGridSize(){
+    let gridSize = prompt("Input a size for the grid. 1 -100")
+    if (+gridSize > 0 && +gridSize <= 100){
+        return gridSize;
+    } else {
+        getGridSize();
+    }
 }
 
-function createGrid(gridSize){
-    let columns = gridSize;
+function deleteGrid(){
+    while (entireGrid.lastElementChild) {
+      entireGrid.removeChild(entireGrid.lastElementChild);
+    }
+}
+
+function createGrid(reset){
+    if (reset = true){
+        deleteGrid();
+    }
+    let columns = getGridSize();
     while (columns > 0){
         let rows = gridSize;
         while (rows > 0){
             const newDiv = document.createElement('div');
             newDiv.classList.add('gridSq');
+            newDiv.style['background-color'] = startColor
             entireGrid.appendChild(newDiv);
             rows--;
         }
         columns--;
     }
+    const gridItems = document.querySelectorAll('.gridSq');
+    gridItems.forEach((e) => {addHover(e)});
 }
 
-createGrid(gridSize);
+createGrid(false);
 
 
-const gridItems = document.querySelectorAll('.gridSq');
-gridItems.forEach((e) => {addHover(e)});
+
 
 const reset = document.querySelector('.reset');
-reset.addEventListener('click', resetGrid);
+reset.addEventListener('click', createGrid);
