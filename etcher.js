9ce411c501a926d1f13428b8cbb230bb7a380272
Tyler <tyler.hagstrom@gmail.com>
2021-09-12 09:46:49 -0500

    Added the etch-a-sketch ability

--- etcher.js
+++ etcher.js
@@ -1,9 +1,16 @@
 let gridSize = 16;
+let color = 'black';
 const entireGrid = document.querySelector('.entireGrid');
 entireGrid.setAttribute('style', 'display:grid;grid-template-columns: repeat('+ gridSize +', 1fr); grid-template-rows: repeat('+gridSize+', 1fr);');
 
+function addHover(item){
+    item.addEventListener('mouseover', (e) => {
+        item.style['background-color'] = color;
+    });
+}
+
+
 function createGrid(gridSize){
-    
     let columns = gridSize;
     while (columns > 0){
         let rows = gridSize;
@@ -18,3 +25,17 @@ function createGrid(gridSize){
 }
 
 createGrid(gridSize);
+
+
+const gridItems = document.querySelectorAll('.gridSq');
+gridItems.forEach((e) => {addHover(e)});
+
+/* gridItems.addEventListener('mouseover', (e) => {
+  console.log(e);
+}); */
+
+
+//make a grid
+//make a hover effect when hovering over a gridsq
+//change a grid sq color when it is hovered over
+//add a button to clear the grid once
